9969f7df6e79467e9febf4eb351ff77baa324931
Naman Singh Rana <nsr@Namans-MacBook-Air.local>
2022-11-22 18:44:32 +0530

    model1

--- Code/model1.py
+++ Code/model1.py
@@ -8,13 +8,15 @@ class MC(nn.Module):
     def __init__(self,input_size) -> None:
         super().__init__()
         self.input_size = input_size
-        self.fc1 = nn.Linear(input_size,50)
-        self.fc2 = nn.Linear(50,50)
-        self.fc3 = nn.Linear(50,10)
+        self.fc1 = nn.Linear(input_size,60)
+        self.fc2 = nn.Linear(60,30)
+        self.fc3 = nn.Linear(30,10)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax()
         self.sigmoid = nn.Sigmoid()
         self.tanh = nn.Tanh()
+        self.drop1 = nn.Dropout(0.05)
+        self.drop2 = nn.Dropout(0.05)
         
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.zeros_(self.fc1.bias)
@@ -22,10 +24,18 @@ class MC(nn.Module):
         nn.init.zeros_(self.fc2.bias)
         nn.init.xavier_uniform_(self.fc3.weight)
         nn.init.zeros_(self.fc3.bias)
+
         
     def forward(self,x):
-        output = self.fc3(self.tanh(self.fc2(self.tanh(self.fc1(x)))))
-        return output
+        x = self.fc1(x)
+        x = self.tanh(x)
+        # x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.tanh(x)
+        # x = self.drop2(x)
+        x = self.fc3(x)
+        # x = self.tanh(x)
+        return x
 
 def train(model,X_trn,Y_trn,X_val,Y_val,epochs=80):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -87,6 +97,6 @@ if __name__ == '__main__':
     Y_val = Y[tc:]
     
     model = MC(X_trn.shape[1])
-    train(model,X_trn,Y_trn,X_val,Y_val,60)
+    train(model,X_trn,Y_trn,X_val,Y_val,80)
     
     
